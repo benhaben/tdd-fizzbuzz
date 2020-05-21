381ca6b4ea386b6e904ae2b77f263bbe07c93b7c
yin <yin.shen@thoughtworks.com>
2020-05-21 22:29:14 +0800

    feat: FizzBuzz logic

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,21 @@ package cn.xpbootcamp.fizzbuzz;
  */
 public class FizzBuzz {
     public String reportNumber(int number) {
-     return "";
+        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
+            return "FizzBuzzWhizz";
+        } else if (number % 5 == 0 && number % 7 == 0) {
+            return "BuzzWhizz";
+        } else if (number % 3 == 0 && number % 7 == 0) {
+            return "FizzWhizz";
+        } else if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 7 == 0) {
+            return "Whizz";
+        }
+        return String.valueOf(number);
     }
 }
